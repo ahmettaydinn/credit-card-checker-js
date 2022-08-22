650ea7bb9868f6f2e612c41d2254c1449ca67d3e
BAVI-BOOP <birceste@gmail.com>
2022-08-22 18:08:14 +0300

    html and css added

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
-
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
-
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
-
-// An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
 
-// Add your functions below:
-function validateCred(numArr) {
+
+
+function validateCred(numbers) {
+  let numArr = numbers.join("").split("")
   let total = 0;
   for (let i = numArr.length - 1; i >= 0; i--) {
-    let currValue = numArr[i]
+    let currValue = Number(numArr[i])
     if ((numArr.length - 1 - i) % 2 === 1) {
       currValue *= 2;
       if (currValue > 9) {
@@ -41,68 +20,94 @@ function validateCred(numArr) {
 
 }
 
-// Test functions:
-console.log(validateCred(valid1)); // Should print true
-console.log(validateCred(invalid1)); // Should print false
-
-function findInvalidCards(cards) {
-  const invalid = [];
-
-  for (let i = 0; i < cards.length; i++) {
-    let currCred = cards[i];
-    if (!validateCred(currCred)) {
-      invalid.push(currCred);
-    }
-  }
-
-  return invalid;
-}
-
-// Test function
-console.log(findInvalidCards([valid1, valid2, valid3, valid4, valid5]));// Shouldn't print anything
-console.log(findInvalidCards([invalid1, invalid2, invalid3, invalid4, invalid5])); // Should print all of the numbers
-
-console.log(findInvalidCards(batch)); // Test what the mystery numbers are
-
-function idInvalidCardCompanies(invalidBatch) {
+function idInvalidCardCompanies(invalid_numbers) {
+  let invalidBatch = invalid_numbers.join("").split("");
   const companies = [];
-  for (let i = 0; i < invalidBatch.length; i++) {
-    switch (invalidBatch[i][0]) {
-      case 3:
-        if (companies.indexOf('Amex') === -1) {
-          companies.push('Amex');
+
+    switch (invalidBatch[0]) {
+      case "3":
+        if (companies.indexOf('amex') === -1) {
+          companies.push('amex');
         }
         break
-      case 4:
-        if (companies.indexOf('Visa') === -1) {
-          companies.push('Visa');
+      case "4":
+        if (companies.indexOf('visa') === -1) {
+          companies.push('visa');
         }
         break
-      case 5:
-        if (companies.indexOf('Mastercard') === -1) {
-          companies.push('Mastercard');
+      case "5":
+        if (companies.indexOf('mastercard') === -1) {
+          companies.push('mastercard');
         }
         break
-      case 6:
-        if (companies.indexOf('Discover') === -1) {
-          companies.push('Discover');
+      case "6":
+        if (companies.indexOf('discover') === -1) {
+          companies.push('discover');
         }
         break
       default:
         console.log('Company not found');
     }
-  }
   return companies;
 }
 
-console.log(idInvalidCardCompanies([invalid1])); // Should print['visa']
-console.log(idInvalidCardCompanies([invalid2])); // Should print ['mastercard']
-console.log(idInvalidCardCompanies(batch)); // Find out which companies have mailed out invalid cards
+
+
+const getValueInput = () =>{
+  let inputValue = document.getElementById("domTextElement").value; 
+  let company = idInvalidCardCompanies([inputValue])
+  let amex =  document.getElementById("amex")
+  let mastercard =  document.getElementById("mastercard")
+  let visa =  document.getElementById("visa")
+  let discover =  document.getElementById("discover")
+  if(validateCred([inputValue])) {
+    document.getElementById("valueInput").innerHTML = `Your ${company} credit card is valid✅`;
+    amex.hidden = true;
+    mastercard.hidden = true;
+    visa.hidden = true;
+    discover.hidden = true;
+    document.getElementById(company).hidden = false
+  } else {
+    amex.hidden = true;
+    mastercard.hidden = true;
+    visa.hidden = true;
+    discover.hidden = true;
+    document.getElementById(company).hidden = false
+    document.getElementById("valueInput").innerHTML = `Your ${company} credit card is invalid❌`;
+
+    
+  }
+   
+  
+}
 
 
 
 
 
+let valid1 = 4539677908016808;
+const valid2 = 5535766768751439;
+const valid3 = [371612019985236];
+
+const valid4 = [6011144340682905];
+console.log(valid4.length)
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+
+const invalid1 = [4532778771091795];
+const invalid2 = [5795593392134643];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5382019772883854];
+
+
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
 
 
